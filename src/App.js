f74e8b232dd9fc11b8fc5c34c6a03ff90088372a
Jaz Gobbo <jazzgobbo@gmail.com>
2022-07-30 13:15:33 -0500

    added text area

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React, { Component} from 'react';
+import React, { Component, useEffect, useState} from 'react';
 import './App.css'
 
-class App extends Component {
-  state = {
-    userInput: ''
-  }
 
-  inputChangedHandler = (event) => {
-    this.setState({userInput : event.target.value});
-  }
+const App = () => {
 
-  render() {
-    return (
+  // store input value in react state
+  const [value, setValue] = useState("")
+
+  // when input changes, store to react state 
+  // and local storage
+  const inputChangedHandler = (e) => {
+    setValue(e.target.value);
+    localStorage.setItem("inputValue", e.target.value);
+  };
+
+  // when component is mounted, set initial state
+  // of input to value in local storage
+  useEffect(() => {
+    setValue(localStorage.getItem("inputValue"));
+  }, []);
+
+  return (
       <div className="App">
-        <input type="text" onChange={this.inputChangedHandler}
-        value={this.state.userInput}/>
-        <div className="texty">
-          <p>{this.state.userInput}</p>
+        <div className="input">
+          <textarea 
+            id="text"
+            name="text" 
+            value={value} 
+            onChange={inputChangedHandler}>
+          </textarea>
         </div>
-     </div>
+        <div className="texty">
+          <p>{value}</p>
+        </div>
+
+      </div>
     );
-  }
-}
+};
+
 export default App;
