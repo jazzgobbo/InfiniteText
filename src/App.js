ab7df6b703c01a28afb9b1d58476b2d03c952f48
Jaz Gobbo <jazzgobbo@gmail.com>
2022-07-30 16:53:05 -0500

    added database functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { FirebaseError } from 'firebase/app';
+import { ref } from 'firebase/database';
 import React, { Component, useEffect, useState} from 'react';
 import './App.css'
+import { useData, setData, database } from './utilities/firebase.js';
 
 
 const App = () => {
 
   // store input value in react state
-  const [value, setValue] = useState("")
+  const [value, setValue] = useState("");
+
+  
+
+  // Push Function pushes value in textarea into database
+  const Push = () => {
+    setData(`/input`, {
+      text : value,
+    }).catch(alert);
+  }
+
 
   // when input changes, store to react state 
   // and local storage
@@ -20,15 +33,28 @@ const App = () => {
     setValue(localStorage.getItem("inputValue"));
   }, []);
 
+
+  window.addEventListener('load', () => {
+    Fetch();
+  });
+
+  const Fetch = () => {
+    var inputRef = database.ref('input');
+    inputRef.on('value').then((snapshot) => {
+      const data = snapshot.val()
+    })
+  }
+
   return (
       <div className="App">
         <div className="input">
           <textarea 
             id="text"
             name="text" 
-            value={value} 
+            value={value}
             onChange={inputChangedHandler}>
           </textarea>
+          <button id="save" onClick={Push}>Save</button>
         </div>
         <div className="texty">
           <p>{value}</p>
---
+++ src/utilities/firebase.js
@@ -0,0 +1,41 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+import React, { Component, useEffect, useState} from 'react';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdqUtp-IMG0RltQdHb5ho6Mw5dygDrxjQ",
+    authDomain: "mythoughtsonascreen.firebaseapp.com",
+    databaseURL: "https://mythoughtsonascreen-default-rtdb.firebaseio.com",
+    projectId: "mythoughtsonascreen",
+    storageBucket: "mythoughtsonascreen.appspot.com",
+    messagingSenderId: "1040905084618",
+    appId: "1:1040905084618:web:aa485cd72ae36862d15e33",
+    measurementId: "G-EE227G9CG6"
+  };
+
+const firebase = initializeApp(firebaseConfig);
+export const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        const dbRef = ref(database, path);
+        const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+        if (devMode) { console.log(`loading ${path}`); }
+        return onValue(dbRef, (snapshot) => {
+          const val = snapshot.val();
+          if (devMode) { console.log(val); }
+          setData(transform ? transform(val) : val);
+        }, (error) => {
+          setData(null);
+        });
+      }, [path, transform]);
+    
+      return [data];
+};
+
+
+export const setData = (path, value) => (
+    set(ref(database, path), value)
+  );
